33130e41fb3d7991ae40ca59d336b4df1a31dcb3
cheraghifar <43294656+cheraghifar@users.noreply.github.com>
2022-08-25 14:24:28 +0300

    Update run2service.sh

--- run2service.sh
+++ run2service.sh
@@ -1,34 +1,40 @@
-echo "[Unit]" > $1.service
-echo Description = $3 >> $1.service
-echo "" >> $1.service
-echo "# Assuming you want to start after network interfaces are made available " >> $1.service
-echo After = network.target >> $1.service
+arg1=$1
+arg2=$2
+arg3=$3
+arg4=$4
+arg5=$5
 
-echo "" >> $1.service
-echo ["Service]" >> $1.service
-echo Type = simple >> $1.service
-echo ExecStart = $2 >> $1.service
+echo "[Unit]" > $arg1.service
+echo Description = $arg3 >> $arg1.service
+echo "" >> $arg1.service
+echo "# Assuming you want to start after network interfaces are made available " >> $arg1.service
+echo After = network.target >> $arg1.service
 
-echo "" >> $1.service
-echo "# User to run the script as " >> $1.service
-echo User = $4 >> $1.service
+echo "" >> $arg1.service
+echo ["Service]" >> $arg1.service
+echo Type = simple >> $arg1.service
+echo ExecStart = $arg2 >> $arg1.service
 
-echo "" >> $1.service
-echo "# Group to run the script as " >> $1.service
-echo Group = $5 >> $1.service
+echo "" >> $arg1.service
+echo "# User to run the script as " >> $arg1.service
+echo User = $arg4 >> $arg1.service
 
-echo "" >> $1.service
-echo "# Restart when there are errors " >> $1.service
-echo Restart = on-failure >> $1.service
+echo "" >> $arg1.service
+echo "# Group to run the script as " >> $arg1.service
+echo Group = $arg5 >> $arg1.service
 
-echo SyslogIdentifier = $1 >> $1.service
-echo RestartSec = 5 >> $1.service
-echo TimeoutStartSec = infinity >> $1.service
+echo "" >> $arg1.service
+echo "# Restart when there are errors " >> $arg1.service
+echo Restart = on-failure >> $arg1.service
 
-echo "" >> $1.service
-echo "[Install]" >> $1.service
+echo SyslogIdentifier = $arg1 >> $arg1.service
+echo RestartSec = 5 >> $arg1.service
+echo TimeoutStartSec = infinity >> $arg1.service
 
-echo "" >> $1.service
-echo "# Make it accessible to other users " >> $1.service
-echo WantedBy = multi-user.target >> $1.service
+echo "" >> $arg1.service
+echo "[Install]" >> $arg1.service
+
+echo "" >> $arg1.service
+echo "# Make it accessible to other users " >> $arg1.service
+echo WantedBy = multi-user.target >> $arg1.service
 
